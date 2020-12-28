f1ae881c84df267f474e108afe37d8d1db304555
guansss <821143943@qq.com>
2020-12-29 05:18:27 +0800

    Init repo

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+});
+</script>
