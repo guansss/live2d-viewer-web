755e5c0bde9e5ec28a30f2cca23787119989a93d
guansss <821143943@qq.com>
2021-01-06 08:44:29 +0800

    Set up model when loaded

--- src/live2d/Live2DModel.ts
+++ src/live2d/Live2DModel.ts
@@ -9,7 +9,51 @@ async function createFrom(url: string): Promise<Live2DModel> {
 }
 
 export class Live2DModel extends _Live2DModel {
+    hitAreaFrames!: HitAreaFrames;
 
+    constructor() {
+        super();
+
+        this.once('modelLoaded', this._init);
+    }
+
+    _init() {
+        this.hitAreaFrames = new HitAreaFrames();
+        this.addChild(this.hitAreaFrames);
+
+        this.anchor.set(0.5, 0.5);
+
+        this.on('hit', this.startHitMotion);
+
+        // TODO: remove this when upgrading pixi-live2d-display to beta.3
+        (this.internalModel.motionManager as any).queueManager.setEventCallback?.(
+            (caller: any, eventValue: string) => {
+                this.emit('motion:' + eventValue);
+            },
+        );
+    }
+
+    startHitMotion(hitAreaNames: string[]) {
+        for (let area of hitAreaNames) {
+            area = area.toLowerCase();
+
+            const possibleGroups = [
+                area,
+                'tap' + area,
+                'tap_' + area,
+                'tap',
+            ];
+
+            for (const possibleGroup of possibleGroups) {
+                for (let group of Object.keys(this.internalModel.motionManager.definitions)) {
+                    if (possibleGroup === group.toLowerCase()) {
+                        this.motion(group);
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
 
 Live2DModel.from = createFrom as any;
